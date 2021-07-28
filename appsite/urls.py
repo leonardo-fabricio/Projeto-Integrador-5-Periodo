d8608d895bc9de0b5de00f96a1a7096a9e8929ae
luis-luciqm <redbullfc18@gmail.com>
2021-07-28 20:26:07 -0300

    Inserindo testes em rotas e views

--- appsite/urls.py
+++ appsite/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.urls.conf import re_path
-from appsite.views import index
+from appsite.views import * # importar tudo para evitar erros
 from django.views.generic.base import RedirectView
 from django.conf.urls import url
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    # path('teste', teste, name='teste')
 ]
--- appsite/views.py
+++ appsite/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index (request):
     return render(request,'index.php')
+
+# def teste (request):
+#     return render(request, 'teste.php')
