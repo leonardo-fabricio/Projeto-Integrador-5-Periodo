cdae9eb5ca17711b085be554de1afb85f602e635
luis-luciqm <redbullfc18@gmail.com>
2021-09-07 14:38:13 -0300

    linkagem de contato via email

--- appsite/views.py
+++ appsite/views.py
@@ -284,7 +284,6 @@ def baixarPdf(request, idevento):
         evento          = Eventos.objects.get(id = idevento)
         listaPessoas    = Publico_Eventos.objects.select_related('idPessoa').filter(idEvento = idevento)
         data = {'evento': evento, 'pessoas':listaPessoas}
-        counter = listaPessoas.count()
 
         #html = template.render(data)
         pdf = render_to_pdf('../templates/arquivoPDF.html', data)
